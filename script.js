b503e02d9fa047986f00b6fffeba10d312029ae9
readingmkorea <readingm01@naver.com>
2025-07-15 13:43:51 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+(() => {
+  const keys = ['s1','s2','s3','v1','v2','v3','c1','c2','c3','c4'];
+  const ctxD = document.getElementById('myChart').getContext('2d');
+  const ctxM = document.getElementById('monthCvs').getContext('2d');
+  let chartD = null, chartM = null;
+
+  // 초기값 세팅, 이벤트 리스너 등
+  // (기존 HTML 안 <script> 에 있는 코드 전체 복사)
+})();
